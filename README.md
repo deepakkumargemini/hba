84609d9bbf0db9ac929d6e867482298de80b19ef
Deepak <deepak.kumar@geminisolutions.in>
2018-11-12 10:25:31 +0530

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# birthday-countdown-js
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/bc159c32969e6c348d0a/maintainability)](https://codeclimate.com/github/yamanoku/birthday-countdown-js/maintainability)
+
+<img src="https://i.gyazo.com/1108059c9f73140f98e6291ef8e002b9.gif" alt="" width="500"/>
+<img src="https://i.gyazo.com/0f6a40e48e9142ea67581d2653e1cb8c.png" alt="" width="500"/>
+
+## Setup
+
+``` bash
+# install dependencies
+yarn（npm install）
+
+# serve with hot reload at localhost:3000
+yarn serve（npm run serve）
+
+# lint for production
+yarn lint（npm run lint）
+
+# babel compiler es5
+yarn build（npm run build）
+```
---
+++ src/countdown.js
@@ -0,0 +1,69 @@
+class CountdownTimer {
+    constructor() {
+        this.initialize(...arguments);
+    }
+    initialize(tl) {
+        this.tl = tl;
+    }
+    countDown() {
+        const today = new Date();
+        const day = Math.floor((this.tl - Number(today)) / (24 * 60 * 60 * 1000));
+        const hour = Math.floor(((this.tl - Number(today)) % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+        const min = Math.floor(((this.tl - Number(today)) % (24 * 60 * 60 * 1000)) / (60 * 1000)) % 60;
+        const sec = Math.floor(((this.tl - Number(today)) % (24 * 60 * 60 * 1000)) / 1000) % 60 % 60;
+        if ((this.tl - Number(today)) > 0 || (this.tl - Number(today)) < -86400000) {
+            let timer = '';
+            timer += `
+            <p>Soon to come.... Yipieee</p>
+            <div class="number">
+                <p>
+                    <span class="number-day">${day}</span>
+                    <span class="number_value">day</span>
+                </p>
+                <p>
+                    <span class="number-hour">${this.addZero(hour)}</span>
+                    <span class="number_value">hour</span>
+                </p>
+                <p>
+                    <span class="number-min">${this.addZero(min)}</span>
+                    <span class="number_value">min</span>
+                </p>
+                <p>
+                    <span class="number-sec">${this.addZero(sec)}</span>
+                    <span class="number_value">sec</span>
+                </p>
+            </div>`;
+            document.getElementById('CDT').innerHTML = timer;
+            setTimeout(() => {
+                this.countDown();
+            }, 10);
+        } else {
+            let mes = '';
+            mes += `
+            <p class="birth_end">Its your birthday :)</p>
+            <a class="birth_btn" href="http://amzn.asia/cti4d0v" target="_blank">
+                For You
+            </a>
+            <a class="birth_btn" href="http://amzn.asia/8Kh4dGA" target="_blank">
+                Here we go
+            </a>
+            `;
+            document.getElementById('CDT').innerHTML = mes;
+            return;
+        }
+    }
+    addZero(num) {
+        return (`0${num}`).slice(-2);
+    }
+}
+
+window.onload = () => {
+    const now = new Date();
+    let tl = new Date(now.getFullYear(), 11, 13);
+    if ((Number(tl) - Number(now)) > 0) {
+        console.log('ins ')
+        tl = new Date(now.getFullYear(), 10, 13);
+    }
+    const timer = new CountdownTimer(tl);
+    timer.countDown();
+};
